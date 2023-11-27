7c1eb9a1e4a26ab4c5a558f91da02cd9ebdbdde3
cegano <118409496+cegano@users.noreply.github.com>
2023-11-27 16:57:03 -0500

    Final Project

---
+++ Project/so_script.js
@@ -0,0 +1,12 @@
+/* File: so_script.js */
+
+
+window.onload = init;
+
+function init() {
+   document.forms[0].onsubmit = function() {
+      if (this.checkValidity()) alert("Data passes initial validation tests");
+      return false;
+   }
+   
+}
